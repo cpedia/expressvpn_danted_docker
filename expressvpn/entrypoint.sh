c984e2e9acfa8ca2be380bd1d4da12632dd65cfe
Ping Chen <cpedia@gmail.com>
2022-08-30 03:14:03 +0800

    new openvpn3 client.

--- expressvpn/entrypoint.sh
+++ expressvpn/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
 
+# Add a route for local network to use the proxy
+gw=$(ip route | awk '/default/ {print $3}')
+ip route add ${LOCAL_NETWORK} via $gw dev eth0
+
 cp /etc/resolv.conf /tmp/resolv.conf
 su -c 'umount /etc/resolv.conf'
 cp /tmp/resolv.conf /etc/resolv.conf
@@ -11,10 +15,6 @@ expressvpn preferences set network_lock off
 expressvpn preferences set preferred_protocol $PREFERRED_PROTOCOL
 expressvpn preferences set lightway_cipher $LIGHTWAY_CIPHER
 expressvpn connect $SERVER
-
-# Add a route for local network to use the proxy
-gw=$(ip route | awk '/default/ {print $3}')
-ip route add ${LOCAL_NETWORK} via $gw dev eth0
 
 # chmod +x /etc/init.d/danted
 # update-rc.d danted defaults
