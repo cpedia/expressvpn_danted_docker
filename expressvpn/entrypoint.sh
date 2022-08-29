52d390c9e4b233fd4310be49c668298b1907e79f
Ping Chen <cpedia@gmail.com>
2022-08-29 22:59:24 +0800

    new openvpn3 client.

--- expressvpn/entrypoint.sh
+++ expressvpn/entrypoint.sh
@@ -12,8 +12,13 @@ expressvpn preferences set preferred_protocol $PREFERRED_PROTOCOL
 expressvpn preferences set lightway_cipher $LIGHTWAY_CIPHER
 expressvpn connect $SERVER
 
+# Add a route for local network to use the proxy
+gw=$(ip route | awk '/default/ {print $3}')
+ip route add ${LOCAL_NETWORK} via $gw dev eth0
+
 # chmod +x /etc/init.d/danted
 # update-rc.d danted defaults
 # service danted restart
 service shadowsocks-libev restart
+
 exec "$@"
